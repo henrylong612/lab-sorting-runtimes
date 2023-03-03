94cd102b5d0d11eebcd8884dd1bdf8c21a79e233
Henry Long <henrylong612@gmail.com>
2023-03-03 10:45:16 -0800

    added table

--- runtimes.py
+++ runtimes.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+from tabulate import tabulate
 '''
 A simple program for measuring the runtime of sorting algorithms.
 '''
@@ -19,6 +20,8 @@ if __name__ == '__main__':
     parser.add_argument('--max_x', type=int, default=0)
     parser.add_argument('--input', choices=['sorted', 'random'], default='random')
     args = parser.parse_args()
+
+    print(" | len(xs) | timsort | merge_sorted | quicksort |")
 
     # perform the runtime tests
     for x in range(0, args.max_x+1):
@@ -52,4 +55,5 @@ if __name__ == '__main__':
         # You will have to look up how to do this formatting.
         # In order to get a proper markdown table,
         # you will have to also print a header line somewhere else.
-        print(f'len(xs)=2**{x} runtimes={runtimes}')
+        
+        print(f"| 2**{x} | {runtimes['timsort']:0.2e} | {runtimes['merge_sorted']:0.2e} | {runtimes['quick_sorted']:0.2e} |")
